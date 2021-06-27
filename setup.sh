bf74c8d3b90df22871e701122ef4c89f7bce2845
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-06-27 23:00:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,16 +7,16 @@ export PATH=$PWD:$PATH
 chmod 777 /app/gclone
 
 #Inject Rclone config
-wget -q https://github.com/Kwok1am/rclone-ac/raw/main/accounts.rar
+wget -q https://github.com/begulatuk/heroku_download/raw/master/accounts.rar
 wget -q https://www.rarlab.com/rar/rarlinux-x64-5.9.0.tar.gz
 tar xf rarlinux-x64-5.9.0.tar.gz
 export PATH=$PWD/rar:$PATH
 unrar -p"${SA_SECRET}" e accounts.rar /app/accounts/
 
-# Install aria2c static binary
-wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
+# Install aria2c static binaryaria2-1.35.0-static-linux-amd64.tar.gz
+wget -q https://github.com/P3TERX/Aria2-Pro-Core/releases/download/[version]/aria2-1.35.0-static-linux-amd64.tar.gz
+tar zxvf aria2-1.35.0-static-linux-amd64.tar.gz
+export PATH=$PWD:$PATH
 
 # Create download folder
 mkdir -p downloads
@@ -26,5 +26,16 @@ wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
 # Tracker
-tracker_list=$(wget -qO- https://trackerslist.com/all.txt |awk NF|sed ":a;N;s/\n/,/g;ta")
+file="trackers.txt"
+echo "$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt)" > trackers.txt
+echo "$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt)" >> trackers.txt
+tmp=$(sort trackers.txt | uniq) && echo "$tmp" > trackers.txt
+sed -i '/^$/d' trackers.txt
+sed -i ":a;N;s/\n/,/g;ta" trackers.txt
+tracker_list=$(cat trackers.txt)
+if [ -f $file ] ; then
+    rm $file
+
+fi
+echo "trackers added"
 echo "bt-tracker=$tracker_list" >> aria2c.conf
